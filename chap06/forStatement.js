76b8071510d6461c83c47bc84e4d5cc2796d0578
EC2 Default User <ec2-user@ip-172-31-34-8.ap-northeast-1.compute.internal>
2024-07-09 14:13:54 +0000

    java

---
+++ chap06/forStatement.js
@@ -0,0 +1,6 @@
+let ans = 0 ;
+for(let i =1;i<=100;i++){
+    ans+= i
+}
+console.log("答え:"+ans);
+// console.log("ループの終了後のi:"+i);
---
+++ chap06/whileAndDowhile.js
@@ -0,0 +1,13 @@
+console.log("whileループ開始");
+let i = 1;
+while(i<0){
+    console.log("iの値:"+i);
+}
+console.log("whileループ終了");
+
+console.log("do-whileループ開始");
+let j = i;
+do{
+    console.log("jの値:"+j);
+}while(j<0);
+console.log("do-whileループ終了");
