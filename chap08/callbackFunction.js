bade70d85f26f39488910103cfb7a5a2e164184b
EC2 Default User <ec2-user@ip-172-31-34-8.ap-northeast-1.compute.internal>
2024-07-14 12:08:22 +0000

    javascript

---
+++ chap08/callbackFunction.js
@@ -0,0 +1,11 @@
+function concatenateSpace(lastName,firstName){
+    return lastName + ""+firstName;
+}
+
+function useConcatenate(name,func){
+    let concatName = func(...name);
+    console.log("結合結果:"+concatName);
+}
+
+let nameParam=["中田","雄二"];
+useConcatenate(nameParam,concatenateSpace);
---
+++ chap08/forEachCallback.js
@@ -0,0 +1,6 @@
+function printElement(currentValue,index,array){
+    console.log((index+1)+"個めの値:"+currentValue);
+}
+
+let list = [2,7,66,4,9];
+list.forEach(printElement);
